767ceed90cb3c6a8953edadf2da1d6f55b446781
DoyleAaron <aarondoyler101@gmail.com>
2023-03-27 18:11:58 +0100

    test

--- src/me/aaron/Customer.java
+++ src/me/aaron/Customer.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.BorderFactory;
 import java.awt.*;
+import javax.swing.*;
 
 
 public class Customer {
@@ -12,6 +13,9 @@ public class Customer {
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(0, 1));
+        JTextField firstNameField = new JTextField();
+        String firstName = firstNameField.getText();
+
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
