217ea0a6928d82d1203ac37f6438f5e985d86573
DoyleAaron <aarondoyler101@gmail.com>
2023-04-03 12:33:02 +0100

    Stock Update Completed

--- src/me/aaron/StockUpdate.java
+++ src/me/aaron/StockUpdate.java
@@ -19,6 +19,7 @@ public class StockUpdate extends JPanel{
     private JTextField itemPriceField;
     private JButton submitButton;
     private JButton menuButton;
+    PreparedStatement pstat = null;
 
     public StockUpdate(){
         JFrame frame = new JFrame("Update Stock Details");
@@ -54,9 +55,9 @@ public class StockUpdate extends JPanel{
 
         try {
             PreparedStatement stockIDQuery = Main.sql.prepareStatement("SELECT stockID FROM Stock");
-            // This query is used to get all of the stock IDs from the stock table
+            // This query is used to get all the stock IDs from the stock table
             ResultSet stockIDs = stockIDQuery.executeQuery();
-            // This is putting all of the stock IDs into a result set called stockIDs
+            // This is putting all the stock IDs into a result set called stockIDs
 
             while (stockIDs.next()) {
                 int stockID = stockIDs.getInt("stockID");
@@ -89,5 +90,44 @@ public class StockUpdate extends JPanel{
         add(menuButton, gbc);
         // Return to menu button
 
+        submitButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent stockUpdateEvent) {
+                String itemName = itemNameField.getText();
+                String itemQuantity = itemQuantityField.getText();
+                String itemPrice = itemPriceField.getText();
+                //This is getting the values from the text fields and assigning them to variables
+                pstat = Main.sql.prepareStatement("UPDATE Stock SET itemName = ?, itemQuantity = ?, itemPrice = ? WHERE stockID = ?");
+                //This is the SQL query for updating the stock information in the table
+                try {
+                    pstat.setString(1, itemName);
+                    pstat.setString(2, itemQuantity);
+                    pstat.setString(3, itemPrice);
+                    pstat.setInt(4, (int) stockIDField.getSelectedItem());
+                    pstat.executeUpdate();
+                    //This is assigning the values from the text fields for the SQL query
+                    JOptionPane.showMessageDialog(StockUpdate.this, "Stock Data updated successfully.");
+                    itemNameField.setText("");
+                    itemQuantityField.setText("");
+                    itemPriceField.setText("");
+                    //This is clearing the text fields after the data has been updated
+                } catch (SQLException stockUpdateException) {
+                    stockUpdateException.printStackTrace();
+                }
+            }
+        });
+
+        menuButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent stockMenuButtonEvent) {
+                JFrame currentFrame = (JFrame) SwingUtilities.getWindowAncestor((JComponent) stockMenuButtonEvent.getSource());
+                currentFrame.dispose();
+                JFrame frame = new JFrame("Stock Menu");
+                StockMenu StockMenu = new StockMenu();
+                frame.add(StockMenu);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.pack();
+                frame.setVisible(true);
+            }
+            // This is the action button for the menu button which closes the current window and loads the customer menu
+        });
     }
 }
